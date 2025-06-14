8f7c4f8f2a39af247254f0efe2891b54261b3563
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-06-14 09:11:56 +0000

    install packages

--- web-app/convex/auth.config.ts
+++ web-app/convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_FRONTEND_API_URL,
+      domain: process.env.VITE_CLERK_FRONTEND_API_URL,
       applicationID: 'convex',
     },
   ],
